2210d740ffa7b74f1a7f6f9b3fe418e0f948c4ab
aatupitkanen <aatu.pitkanen@gmail.com>
2015-05-22 15:46:03 +0300

    Added image and routes folder

---
+++ routes/image.js
@@ -0,0 +1,19 @@
+'use strict';
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Toimii' });   
+});
+
+app.use('/img', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port); 
