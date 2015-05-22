89a5d3039b0ee7c23cb9de072e17b42e72b5ba8b
nikolauska <nikolauska1@gmail.com>
2015-05-22 14:07:28 +0300

    removed old and added new preliminary code

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-var app  = require('./server');
-var port = process.env.PORT || 9003;
-
-return app.listen(port, function() {
-	console.log('server listening at:', port);
-});
--- utils/webshot.js
+++ utils/webshot.js
@@ -1,25 +1,49 @@
 'use strict';
 
+var webshot  	 = require('webshot');
+var jade 	 	 = require('jade');
 
-var webshot = require('webshot');
+var imageOptions = require('../config/image/options');
 
-module.exports = function(html, dest, callback) {
+function imageExistsCheck(html) {
 
-	var zoomFactor = 0.20;
+}
 
-	var options = {
-		siteType:   'html',
-		zoomFactor: zoomFactor,
-		shotSize: {
-			width:  1920 * zoomFactor,
-			height: 1080 * zoomFactor
-		}
-	}
+function generateImage(type, callback, jadeOptions, webshotOptions) {
+	var jadeOpt = jadeOptions;
+	var webshotOpt = webshotOptions;
+	var optionsDefault = imageOptions.getFromType(type);
 
-	return webshot(html, dest, options, function(err) {
+	// If options weren't given then use default
+	if(typeOf jadeOpt == "undefined") {jadeOpt = optionsDefault.jadeOptions;};
+	if(typeOf webshotOpt == "undefined") {webshotOpt = optionsDefault.webshotOptions;};
+
+	// Where image will be created before saving to db
+	imagePath = '/temp/' + 'id' + 'png';
+
+
+	// Generates html
+	return jade.renderFile(jadePath, jadeOpt, function(err, html) {
 		if(err) {
 			return callback(err);
 		}
-		return callback();
-	});
+
+		// Check if image already exists
+		if(imageExistsCheck(html)) {
+			return callback();
+		}
+
+		// Generate image from html
+		return webshot(html, imagepath, webshotOpt, function(err) {
+			if(err) {
+				return callback(err);
+			}
+
+			return callback();
+		});
+	});	
+}
+
+module.exports = {
+	generateImage: generateImage
 }
