78264e1a8cef9cc16bb37e8338b4b2db9346f476
AatuPitkanen <aatu.pitkanen@gmail.com>
2015-05-29 10:58:15 +0000

    Made little changes to bodyparser

--- config/express.js
+++ config/express.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var app    = require('express')();
-var bodyParser = require('body-parser');
-var config = require('../config');
+var app 		= require('express')();
+var bodyParser 	= require('body-parser');
+var config 		= require('../config');
 
 if(process.env.NODE_ENV == 'development') {
 	app.use(require('morgan')({ format: 'dev' }));
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV == 'development') {
 
 app.set('json spaces', 2);
 
-app.use(bodyParser.urlencoded({ 'limit': '1000kb', 'extended': true }));
+app.use(bodyParser.urlencoded({'extended': true }));
 app.use(bodyParser.json());
 app.use(require('method-override')());
 
