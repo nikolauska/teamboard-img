895ef3289fd5dd32de5821b0e9c8804982b57826
Teemu Kontio <teemu@t-palvelut.net>
2015-06-18 10:43:08 +0300

    Added support for /version route

---
+++ routes/version.js
@@ -0,0 +1,19 @@
+'use strict';
+var Router      = require('express').Router();
+
+Router.route('/')
+    .get(function(req, res) {
+        var version = process.env.img_version || 'unknown';
+        version = '{"version":"'+version+'"}'
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        return res.status(200).json(JSON.parse(version));
+    });
+
+Router.route('/')
+    .options(function(req, res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        return res.status(200).send();
+    });
+module.exports = Router;
